a79f278b35ea9fc0d68d539dffaf181f0b002ac2
qc-jsse-ph13 <qc.jsse.ph13@gmail.com>
2023-06-16 16:02:59 +0800

    Adapted from musikalkemist/generating-melodies-with-rnn-lstm

---
+++ preprocess.py
@@ -0,0 +1,243 @@
+import os
+import json
+import music21 as m21
+import numpy as np
+import tensorflow as tf
+
+KERN_DATASET_PATH = "musicxml"
+SAVE_DIR = "dataset"
+SINGLE_FILE_DATASET = "file_dataset"
+MAPPING_PATH = "mapping.json"
+SEQUENCE_LENGTH = 64  # 64
+ACCEPTABLE_DURATIONS = [
+    0.25,
+    0.5,
+    0.75,
+    1.0,
+    1.5,
+    2,
+    3,
+    4
+]
+
+
+def load_songs(dataset_path):
+    songs = []
+
+    for path, subdir, files in os.walk(dataset_path):
+        for file in files:
+            if file[-3:] == "mxl":
+                song = m21.converter.parse(os.path.join(path, file))
+                songs.append(song)
+    return songs
+
+
+def has_acceptable_duration(song, acceptable_durations):
+    for note in song.flat.notesAndRests:
+        if note.duration.quarterLength not in acceptable_durations:
+            return False
+    return True
+
+
+def transpose(song):
+    # get key from song
+    parts = song.getElementsByClass(m21.stream.Part)
+    measures_part0 = parts[0].getElementsByClass(m21.stream.Measure)
+    key = measures_part0[0][4]
+
+    # estimate key
+    if not isinstance(key, m21.key.Key):
+        key = song.analyze("key")
+
+    # get interval for transposition
+    if key.mode == "major":
+        interval = m21.interval.Interval(key.tonic, m21.pitch.Pitch("C"))
+    elif key.mode == "minor":
+        interval = m21.interval.Interval(key.tonic, m21.pitch.Pitch("A"))
+
+    transposed_song = song.transpose(interval)
+
+    return transposed_song
+
+
+def encode_song(song, time_step=0.25):
+    encoded_song = []
+
+    for event in song.flat.notesAndRests:
+        # e.g. (C, 1), (D, 0.5) -> [60, "_", "_", "_", 62, "_", "_"]
+
+        if isinstance(event, m21.note.Note):
+            if event.tie is not None and event.tie.type == 'stop':
+                symbol = "_"
+            else:
+                symbol = event.pitch.midi
+        elif isinstance(event, m21.note.Rest):
+            symbol = "r"
+
+        steps = int(event.duration.quarterLength / time_step)
+
+        for step in range(steps):
+            if step == 0:
+                encoded_song.append(symbol)
+            else:
+                encoded_song.append("_")
+
+    # cast list to str
+    encoded_song = " ".join(map(str, encoded_song))
+
+    return encoded_song
+
+
+def encode_lyrics(song, time_step=0.25):
+    encoded_lyrics = []
+
+    for event in song.flat.notesAndRests:
+
+        if isinstance(event, m21.note.Note):
+            if event.tie is not None and event.tie.type == 'stop':
+                symbol_lyric = "_"
+            else:
+                symbol_lyric = event.lyric
+
+        elif isinstance(event, m21.note.Rest):
+            symbol_lyric = "r"
+
+        steps = int(event.duration.quarterLength / time_step)
+
+        for step in range(steps):
+            if step == 0:
+                encoded_lyrics.append(symbol_lyric)
+            else:
+                encoded_lyrics.append("_")
+
+    encoded_lyrics = " ".join(map(str, encoded_lyrics))
+
+    return encoded_lyrics
+
+
+def load(file_path):
+    with open(file_path, "r") as fp:
+        song = fp.read()
+    return song
+
+
+def create_single_file_dataset(dataset_path, file_dataset_path, sequence_length):
+    # Every input of LSTM must be of the same length
+    new_song_delimiter = "/ " * sequence_length
+    songs = ""
+
+    # load encoded songs and add delimiters
+    for path, _, files in os.walk(dataset_path):
+        for file in files:
+            file_path = os.path.join(path, file)
+            song = load(file_path)
+            songs = songs + song + " " + new_song_delimiter
+
+    # remove last space
+    songs = songs[:-1]
+
+    with open(file_dataset_path, "w") as fp:
+        fp.write(songs)
+
+    return songs
+
+
+def create_mapping(songs, mapping_path):
+    mappings = {}
+
+    # identify the vocab
+
+    songs = songs.split()
+
+    # e.g. {1 5 3 2 4 1 5 3 5} set-> {1 2 3 4 5}
+    vocabulary = list(set(songs))
+
+    # create mappings
+    for i, symbol in enumerate(vocabulary):
+        mappings[symbol] = i
+
+    # save vocab to json
+    with open(mapping_path, "w") as fp:
+        json.dump(mappings, fp, indent=4)
+
+
+def convert_songs_to_int(songs):
+    int_songs = []
+
+    # load mappings
+    with open(MAPPING_PATH, "r") as fp:
+        mappings = json.load(fp)
+
+    # cast songs string to a list
+    songs = songs.split()
+
+    # map songs to int
+    for symbol in songs:
+        int_songs.append(mappings[symbol])
+
+    return int_songs
+
+
+def generating_training_sequences(sequence_length):
+    # Give the network 4 bars of notes (64 time steps), and ask it to predict the next one
+
+    # load songs
+    songs = load(SINGLE_FILE_DATASET)
+    int_songs = convert_songs_to_int(songs)
+
+    inputs = []
+    targets = []
+
+    # generate the training sequences (e.g. 100 notes -> 36 training samples)
+    num_sequences = len(int_songs) - sequence_length
+    for i in range(num_sequences):
+        inputs.append(int_songs[i:i + sequence_length])
+        targets.append(int_songs[i + sequence_length])
+
+    # one-hot encoding
+    # input shape: (# of sequences, sequence length) -> (# of sequences, sequence length, vocabulary size)
+    vocabulary_size = len(set(int_songs))
+    inputs = tf.keras.utils.to_categorical(inputs, num_classes=vocabulary_size)
+    targets = np.array(targets)
+
+    return inputs, targets
+
+
+def main():
+    preprocess(KERN_DATASET_PATH)
+    # TODO: Adapt these lines of code
+    # songs = create_single_file_dataset(SAVE_DIR, SINGLE_FILE_DATASET, SEQUENCE_LENGTH)
+    # create_mapping(songs, MAPPING_PATH)
+    # inputs, targets = generating_training_sequences(SEQUENCE_LENGTH)
+
+
+def preprocess(dataset_path):
+    pass
+
+    # load the songs
+    print("Loading songs...")
+    songs = load_songs(dataset_path)
+    print(f"Loaded {len(songs)} songs.")
+
+    for i, song in enumerate(songs):
+
+        # filter out songs
+        if not has_acceptable_duration(song, ACCEPTABLE_DURATIONS):
+            continue
+
+        song = transpose(song)
+
+        # encode songs with music time series representation
+        encoded_song = encode_song(song)
+        encoded_lyrics = encode_lyrics(song)
+
+        # save songs to a text file
+        save_path = os.path.join(SAVE_DIR, str(i))
+        with open(save_path, "w") as fp:
+            fp.write(encoded_song)
+            fp.write("\n")
+            fp.write(encoded_lyrics)
+
+
+if __name__ == "__main__":
+    main()
---
+++ train.py
@@ -0,0 +1,46 @@
+import keras.layers
+from preprocess import generating_training_sequences, SEQUENCE_LENGTH
+import tensorflow as tf
+
+# vocab size
+OUTPUT_UNITS = 38
+NUM_UNITS = [256]
+LOSS = "sparse_categorical_crossentropy"
+LEARNING_RATE = 0.001
+EPOCHS = 50
+BATCH_SIZE = 64
+SAVE_MODEL_PATH = "model.h5"
+
+
+def build_model(output_units, num_units, loss, learning_rate):
+
+    input = tf.keras.layers.Input(shape=(None, output_units))
+    x = keras.layers.LSTM(num_units[0])(input)
+    x = keras.layers.Dropout(0.2)(x)
+    output = tf.keras.layers.Dense(output_units, activation="softmax")(x)
+    model = tf.keras.Model(input, output)
+
+    model.compile(loss=loss,
+                  optimizer=tf.keras.optimizers.legacy.Adam(learning_rate=learning_rate),
+                  metrics=["accuracy"])
+    model.summary()
+    return model
+
+
+def train(output_units=OUTPUT_UNITS, num_units=NUM_UNITS, loss=LOSS, learning_rate=LEARNING_RATE):
+
+    # generate the training sequences
+    inputs, targets = generating_training_sequences(SEQUENCE_LENGTH)
+
+    # build the network
+    model = build_model(output_units, num_units, loss, learning_rate)
+
+    # train the model
+    model.fit(inputs, targets, epochs=EPOCHS, batch_size=BATCH_SIZE)
+
+    # save the model
+    model.save(SAVE_MODEL_PATH)
+
+
+if __name__ == "__main__":
+    train()
